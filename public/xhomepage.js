0e0c5ac380ffa21d4edae6106b20ce3cc7fccb21
Naeem-Ullah-Bj <54583655+Naeem-Ullah-Bj@users.noreply.github.com>
2026-01-07 00:23:27 +0500

    Implement homepage date functions in xhomepage.js
    Added initial structure and functions for homepage date handling.

--- public/xhomepage.js
+++ public/xhomepage.js
@@ -1,1 +1,35 @@
-alert();
+(function(){
+var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+function addHomepageDate(box,dateObj){
+}
+
+function lastDayDate(day){
+}
+
+function formatDate(date){
+return("0"+date.getDate()).slice(-2)+"/"+("0"+(date.getMonth()+1)).slice(-2)+"/"+date.getFullYear();
+}
+
+function numbers(day,mtp){
+}
+
+function overlayNumbers(box,nums,dayName,dateStr,mtp,smallSet){
+}
+
+function applyOverlay(post){
+}
+
+function sortValue(post){
+}
+
+function initPosts(){
+}
+
+function handleLoadMore(hiddenPosts){
+}
+
+var postsData=initPosts();
+if(postsData)handleLoadMore(postsData.hiddenPosts);
+
+})();
